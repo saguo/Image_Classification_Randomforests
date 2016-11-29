216c2d9f41422b854aeb053d0ae2b9ac0fcf9b98
saguo <shuanguo2580@gmail.com>
2016-11-28 21:16:11 -0500

    move the entropy function outside of svm_train

--- svm_train.m
+++ svm_train.m
@@ -28,30 +28,18 @@ for i=1:length(QX)
     QLx = QX(:,(Pred_label == 1));
     QRx = QX(:,(Pred_label == 0));
 
-    % compute entropy the left node
-    PQL = hist(data(QLx,size(data,2)),1:clmax)+1e-6;
-    PQL = PQL / sum(PQL);
-    entropyQL = -1*sum(PQL .*log2(PQL));
-    magnitudeQL = length(QLx);
-
-    % compute entropy the right node
-    PQR = hist(data(QRx,size(data,2)),1:clmax)+1e-6;
-    PQR = PQR / sum(PQR);
-    entropyQR = -1*sum(PQR.*log2(PQR));
-    magnitudeQR = length(QRx);
-
-    Gain = entropy - (magnitudeQL * entropyQL + magnitudeQR * entropyQR) / magnitude;
+    [Gain,PQL,PQR,entropyQL,entropyQR] = gain_entropy(entropy,QLx,QRx,data,clmax);
 
     if(BestGain < Gain)
 
-            QLx_ = QLx;
-            QRx_ = QRx;
-            par_ = new_Par;
-            entropyQL_ = entropyQL;
-            entropyQR_ = entropyQR;
-            PQL_ = PQL;
-            PQR_ = PQR;
-            BestGain = Gain;
+      QLx_ = QLx;
+      QRx_ = QRx;
+      par_ = new_Par;
+      entropyQL_ = entropyQL;
+      entropyQR_ = entropyQR;
+      PQL_ = PQL;
+      PQR_ = PQR;
+      BestGain = Gain;
     end
 
 end
@@ -59,7 +47,7 @@ end
 % Determine if split is found
 if (BestGain == th)
     split_found = 0;
-else
+    else
     split_found = 1;
 end
 
