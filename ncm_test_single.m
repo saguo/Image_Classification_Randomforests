0f04be411e5864c71a238c2c50d197a98c563fb6
saguo <shuanguo2580@gmail.com>
2016-12-05 15:35:43 -0500

    update

--- ncm_test_single.m
+++ ncm_test_single.m
@@ -1,17 +1,11 @@
 function direction = ncm_test_single(datapoint, par)
 % 1 for left, 2 for right
 
-    %mahalDistance = 1;    
-
     Pred_class = 0;
     Pred = 0;
     distance = zeros(size(par,1),1);
     for n = 1:length(par)
-        %if mahalDistance
-        %    distance(n) = mean(mahal(datapoint', par(n,1 : size(par,2)-1)')); 
-        %else
             distance(n) = mean(dist2(datapoint, par(n,1 : size(par,2)-1)));
-        %end
     end
     [minvalue, minidx] = min(distance);
     Pred_class = minidx;
