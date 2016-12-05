ce4b82bf4835bad59d20d6131efed5a4a86fdcb6
saguo <shuanguo2580@gmail.com>
2016-12-05 15:10:44 -0500

    ncm_train(dist2)

--- ncm_train.m
+++ ncm_train.m
@@ -1,51 +1,72 @@
 function[QLx_, QRx_, par_, entropyQL_,entropyQR_,PQL_,PQR_, split_found] = ...
     ncm_train(data, QX, entropy, clmax)
 
-  th = 0.001;
+%% NCM Train
+% Combining NCM and Random Forest
+% INPUT : data: whole data  QX: sampled data 
+%         entropy: entropy of parent tree
+%         clmax : number of total classes
+
+
+  th = 0.001; % threshold
   BestGain = th;
+  %mahalDistance = 1; % 1-yes 0-no
+  
+  QLx_ = 0; % left node
+  QRx_ = 0; % right node
+  par_ = 0; % parameter(c_means)
+  entropyQL_ = 0; % left node - entropy
+  entropyQR_ = 0; % right node - entropy
+  PQL_ = 0; % left node - distribution
+  PQR_ = 0; % right node - distribution
 
-  QLx_ = 0;
-  QRx_ = 0;
-  par_ = 0;
-  entropyQL_ = 0;
-  entropyQR_ = 0;
-  PQL_ = 0;
-  PQR_ = 0;
 
-for i=1:length(QX)
+  % Initialization
+  X = data(QX,1:(size(data,2) - 1));
+  class_label = data(QX,size(data,2));
+  magnitude = length(QX);
 
-    X = data(QX,1:(size(data,2) - 1));
-    class_label = data(QX,size(data,2));
-    magnitude = length(QX);
-
-    c_mean = zeros(clmax,size(X,2));
+  
+  % Calculation of centroids : c_means
+  
+  c_mean = zeros(clmax,size(X,2));
     
-    for j = 1:clmax
-        c_data = X(class_label == j,:);
-        c_mean(j,:) = sum(c_data,1)/size(c_data,1);
-    end
+  for j = 1:clmax
+      c_data = X(class_label == j,:);
+      c_mean(j,:) = sum(c_data,1)/size(c_data,1);
+  end
+   
+  
+  % predict classes for each datapoint
+  Pred_class = zeros(magnitude,1);
+  distance = zeros(clmax,1);
+  for m =1: magnitude
+      for n = 1:clmax
+          %if mahalDistance
+          %  distance(n) = mean(mahal(X(m,:)', c_mean(n,:)'));
+          %else
+            distance(n) = mean(dist2(X(m,:), c_mean(n,:)));
+          %end
+      end
+      [minvalue, minidx] = min(distance);
+      Pred_class(m) = minidx;
+      
+  end
+    
+  for i=1:length(QX)
+        
+    Pred_label = zeros(magnitude,1);
+    
     
     assign_label = ones(clmax,1);
     rand_idx = randperm(clmax, round(clmax * 0.5));
-    assign_label(rand_idx) = 0;
-    
-    
-    Pred_class = zeros(magnitude,1);
-    Pred_label = zeros(magnitude,1);
-    distance = zeros(clmax,1);
-    
+    assign_label(rand_idx) = 0;  
     
     for m =1: magnitude
-        for n = 1:clmax
-            distance(n) = mean(mahal(X(m,:)', c_mean(n,:)'));
-        end
-        [minvalue, minidx] = min(distance);
-        Pred_class(m) = minidx;
         Pred_label(m) = assign_label(Pred_class(m));
     end
-    
-    new_Par = c_mean;
-    
+      
+    new_Par = [c_mean assign_label];
     QLx = QX(:,(Pred_label == 1));
     QRx = QX(:,(Pred_label == 0));
 
@@ -63,13 +84,13 @@ for i=1:length(QX)
       BestGain = Gain;
     end
 
-end
+  end
 
 % Determine if split is found
-if (BestGain == th)
-    split_found = 0;
-    else
-    split_found = 1;
-end
+  if (BestGain == th)
+      split_found = 0;
+  else
+      split_found = 1;
+  end
 
 end
