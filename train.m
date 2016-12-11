6f90d8abf7551e552b01432495d473a1f9bc0fe5
saguo <saguo@umich.edu>
2016-12-11 18:10:57 -0500

    train

--- train.m
+++ train.m
@@ -10,9 +10,12 @@ function sroot = train(data, clmax, mmax, depthmax, train_func)
 Qx = 1: size(data, 1);
 sroot = cell(mmax, 1);
 for i = 1: mmax
-    [PQ, entropyQ] = entropy(data, Qx, clmax); % compute the entropy of root
+    PQ=hist(data(Qx,size(data,2)),1:clmax)+1e-6;
+    PQ=PQ/sum(PQ);
+    entropyQ=-1*sum(PQ.*log2(PQ)); % compute the entropy of root
     root = branch(Qx, 0, length(Qx), entropyQ, PQ);
-    sroot{i} = grow(root, data, depthmax, clmax, train_func); 
+    sroot{i} = grow(root, data, depthmax, clmax, train_func);
+    save(strcat('tree',sprintf('%02d.mat',i)),'root');
 end
 
 
