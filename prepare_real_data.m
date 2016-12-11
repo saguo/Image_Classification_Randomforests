550c812847cc88c17e1d4241be800bce7d1b48d5
saguo <saguo@umich.edu>
2016-12-11 12:58:18 -0500

    prepare_real_data

---
+++ prepare_real_data.m
@@ -0,0 +1,21 @@
+function [data_train, data_test] = prepare_real_data(clmax)
+
+addpath('features')
+
+load 'features.mat'
+
+% get first n classes of images
+
+train_num = 500;
+test_num = 100;
+
+train_x_n = train_x(:, 1: clmax * train_num);
+train_y_n = train_y(:, 1: clmax * train_num);
+
+test_x_n = test_x(:, 1: clmax * test_num);
+test_y_n = test_y(:, 1: clmax * test_num);
+
+
+data_train = [train_x_n' train_y_n'];
+
+data_test = [test_x_n',test_y_n'];
