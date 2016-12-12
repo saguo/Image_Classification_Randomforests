89bfdb5fc5fe30694e6a52ae933d7f14cec374b7
saguo <saguo@umich.edu>
2016-12-12 12:36:29 -0500

    main

--- main.m
+++ main.m
@@ -1,17 +1,17 @@
 clear
 clmax = 5; % number of maximum classes
-initsize = 5 ; % number of initial classes
-nmax = 600; % population of each class
-trainmax = 500; % max population of each training class; realdata fixed:500
-testmax = 100; % max population of each test class; realdata fixed:100
+initsize = 2 ; % number of initial classes
+nmax = 60; % population of each class
+trainmax = 50; % max population of each training class; realdata fixed:500
+testmax = 10; % max population of each test class; realdata fixed:100
 mmax = 10; % number of trees
 depthmax = 10; % maximum depth for each tree
 ratio = 0.3; 
 incre_func = @RTSTQ; % incremental learning method
 train_func = @svm_train; % split_train or ncm_train or svm_train
 test_func = @svm_test;
-increment = 0; % increment -1 static - 0
-realdata = 1;   % realdata - 1 spiraldata - 0  
+increment_m = 1; % increment -1 static - 0
+realdata = 0;   % realdata - 1 spiraldata - 0  
 
 if realdata
     [data_train, data_test] = prepare_real_data(initsize);
@@ -30,7 +30,7 @@ else
     
 end
 
-if increment
+if increment_m
     % format increment data
     data_incre = data_train(initsize*trainmax+1: end, :);
     data_incre = data_incre(randperm(size(data_incre, 1)), :);
@@ -39,6 +39,7 @@ if increment
     
     
     %train
+    sroot = train(data_train, initsize, mmax, depthmax, train_func);
     sroot = increment(sroot, data_combined, Qx, clmax, depthmax, ratio, incre_func, train_func, test_func);
     [~, incre_error] = test(sroot, data_combined, clmax, test_func);
 else
