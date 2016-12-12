5ae65d7da3e467b5d243167c816038d67b4fd5d6
Philo Zhang <philodev8@outlook.com>
2016-12-12 17:22:53 -0500

    one click output, you can not ask more

--- main.m
+++ main.m
@@ -8,7 +8,7 @@ source = 0; % 0 for spiral data, else for real data
 
 if source == 0
     % prepare spiral data using previously defined "train_num, test_num, class_num"
-    train_num = 80; % train_num: polpulation of each class for training
+    train_num = 100; % train_num: polpulation of each class for training
     test_num = 20; % polpulation of each class for testing
     class_num = 10; % class_num: class number of all data
     [data_train, data_test] = prepare_spiral_data(class_num, train_num, test_num); % spiral data
@@ -17,10 +17,10 @@ else
     % so if you define these three variables before, they will be overwritten by mine, just in case 
     % some careless assign invalid values. But it's fine that you redefine "class_num" after loading data.
     load('features/real.mat'); 
-    class_num = 10; % just for short time test
+    class_num = 6; % just for short time test
 end
 
-init_size = 4; % init_size: class number of initial training data
+init_size = 6; % init_size: class number of initial training data
 incre_size = 2; % incre_size: class number of each increment
 forest_size = 10; % forest_size: number of trees
 depthmax = 20; % depthmax: maximum tree depth
